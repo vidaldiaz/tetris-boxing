ca50ed5569c9ebc4a9b2c819772becce196e6b1a
Vidal Diaz <vgdiaz@hotmail.com>
2020-04-08 17:51:12 -0500

    Backup antes de agregar el movimiento fluido....Se agregan brazos a los boxers

--- js/main.js
+++ js/main.js
@@ -37,7 +37,91 @@ class BoxerP1 {
   moveRight() {
     this.x += this.speed
   }
+
+  drawArmUp() {
+    context.fillRect(this.x + this.width - 10, this.y + 3, 10, 10)
+  }
+  drawArmDown() {
+    context.fillRect(this.x + this.width - 10, this.y + this.height - 12, 10, 10)
+  }
+
+  expandUp() {
+    this.scope = 0
+    this.scope += 100
+    context.fillStyle = '#EA3C63'
+    context.fillRect(this.x + this.width - 10, this.y + 3, 10 + this.scope, 10)
+  }
+
+  expandDown() {
+    this.scope = 0
+    this.scope += 100
+    context.fillStyle = '#EA3C63'
+    context.fillRect(this.x + this.width - 10, this.y + this.height - 12, 10 + this.scope, 10)
+  }
+
+  // isTouching(x, y, w, h) {
+  //   return (
+  //     this.x < pipe.x + pipe.width &&
+  //     this.x + this.width > pipe.x &&
+  //     this.y < pipe.y + pipe.height &&
+  //     this.y + this.height > pipe.y
+  //   )
+  // }
 }
+//Codigo para Boxer Azul
+// class BoxerP2 {
+//   constructor(x, y, width, height, attack, scope, defese, healPoints, speed, imageSrc) {
+//     this.x = x
+//     this.y = y
+//     this.width = width
+//     this.height = height
+//     this.attack = attack
+//     this.scope = scope
+//     this.defese = defese
+//     this.healPoints = healPoints
+//     this.speed = speed
+//     this.boxerImage = new Image()
+//     this.boxerImage.src = imageSrc
+//     this.armUpArea = 0
+//     this.armDownArea = 0
+//   }
+
+//   moveUp() {
+//     this.y -= this.speed
+//   }
+//   moveDown() {
+//     this.y += this.speed
+//   }
+//   moveLeft() {
+//     this.x -= this.speed
+//   }
+//   moveRight() {
+//     this.x += this.speed
+//   }
+
+//   drawArmUp() {
+//     context.fillRect(this.x + 2, this.y + 2, 10, 10)
+//   }
+//   drawArmDown() {
+//     context.fillRect(this.x + 2, this.y + this.width + 10, 10, 10)
+//   }
+
+//   expandUp() {
+//     this.scope = 0
+//     this.scope += 100
+//     context.fillStyle = '#3EAAF4'
+//     context.fillRect(this.x + 2, this.y + 2, 10 - this.scope, 10)
+
+//     return { x: this.x, y: this.y + 2, w: 10 - this.scope, h: 10 }
+//   }
+
+//   expandDown() {
+//     this.scope = 0
+//     this.scope += 100
+//     context.fillStyle = '#3EAAF4'
+//     context.fillRect(this.x + 2, this.y + this.width + 10, 10 - this.scope, 10)
+//   }
+// }
 
 class BoxerP2 {
   constructor(x, y, width, height, attack, scope, defese, healPoints, speed, imageSrc) {
@@ -52,6 +136,8 @@ class BoxerP2 {
     this.speed = speed
     this.boxerImage = new Image()
     this.boxerImage.src = imageSrc
+    this.armUpArea = 0
+    this.armDownArea = 0
   }
 
   moveUp() {
@@ -66,17 +152,45 @@ class BoxerP2 {
   moveRight() {
     this.x += this.speed
   }
+
+  drawArmUp() {
+    context.fillRect(this.x + 2, this.y + 4, 10, 10)
+  }
+  drawArmDown() {
+    context.fillRect(this.x + 2, this.y + this.width + 7, 10, 10)
+  }
+
+  expandUp() {
+    this.scope = 0
+    this.scope += 100
+    context.fillStyle = 'black'
+    context.fillRect(this.x + 10, this.y + 4, 10 - this.scope, 10)
+
+    return { x: this.x, y: this.y + 2, w: 10 - this.scope, h: 10 }
+  }
+
+  expandDown() {
+    this.scope = 0
+    this.scope += 100
+    context.fillStyle = 'black'
+    context.fillRect(this.x + 10, this.y + this.width + 7, 10 - this.scope, 10)
+    return { x: this.x + 10, y: this.y + this.width + 7, w: 10 - this.scope, h: 10 }
+  }
 }
 
 let p1Boxer = new BoxerP1(20, 20, 60, 80, 8, 'large', 4, 80, 5, images.boxerRedP1)
-//let p2Boxer = new BoxerP2(620, 360, 80, 100, 10, 'medium', 8, 100, 3, images.boxerBlackP2)
-let p2Boxer = new BoxerP2(620, 360, 70, 90, 12, 'slow', 6, 90, 4, images.boxerBlueP2)
+let p2Boxer = new BoxerP2(620, 360, 80, 100, 10, 'medium', 8, 100, 3, images.boxerBlackP2)
+//let p2Boxer = new BoxerP2(620, 360, 70, 90, 12, 'slow', 6, 90, 4, images.boxerBlueP2)
 
 //Funciones Principales
 function update() {
   context.clearRect(0, 0, canvas.width, canvas.height)
   drawP1Boxer(p1Boxer)
   drawP2Boxer(p2Boxer)
+  //p1Boxer.drawArmUp()
+  //p1Boxer.drawArmDown()
+  // p2Boxer.drawArmUp()
+  // p2Boxer.drawArmDown()
 }
 
 setInterval(update, 1000 / 60)
@@ -92,7 +206,7 @@ function drawP2Boxer() {
 //Eventos
 document.addEventListener('keydown', (e) => {
   keydown = e.keyCode
-  console.log(keydown)
+  //console.log(keydown)
   switch (keydown) {
     case 87: //P1 Up
       return p1Boxer.moveUp()
@@ -106,6 +220,12 @@ document.addEventListener('keydown', (e) => {
     case 68: //P1 Right
       return p1Boxer.moveRight()
       break
+    case 84: // P1 Arm Up
+      return p1Boxer.expandUp()
+      break
+    case 71: // P1 Arm Down
+      return p1Boxer.expandDown()
+      break
     case 38: //P2 Up
       return p2Boxer.moveUp()
       break
@@ -118,5 +238,30 @@ document.addEventListener('keydown', (e) => {
     case 39: //P2 Right
       return p2Boxer.moveRight()
       break
+    case 79: // P2 Arm Up
+      return p2Boxer.expandUp()
+      break
+    case 76: // P2 Arm Down
+      return p2Boxer.expandDown()
+      break
   }
 })
+
+// document.addEventListener('keyup', (e) => {
+//   keyup = e.keyCode
+//   //console.log(keyup)
+//   switch (keyup) {
+//     // case 84: // P1 Arm Up
+//     //   return p1Boxer.expandUp()
+//     //   break
+//     // case 71: // P1 Arm Down
+//     //   return p1Boxer.expandDown()
+//     //   break
+//     // case 79: // P2 Arm Up
+//     //   return p2Boxer.expandUp()
+//     //   break
+//     case 76: // P2 Arm Down
+//       return p2Boxer.returnDown()
+//       break
+//   }
+// })

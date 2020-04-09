efd44745550987527829539d27899fee3ad51d03
Vidal Diaz <vgdiaz@hotmail.com>
2020-04-09 13:03:18 -0500

    Version 1 funcional 100%, falta poner limites de pantalla y parar el juego cuando haya KO

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ const friction = 0.8
 const keys = []
 
 let p1Selected = 'red'
-let p2Selected = 'black'
-//let p2Selected
+let p2Selected = 'blue'
+
+let percentageHpP1 = 100
+let percentageHpP2 = 100
 
 const images = {
   boxerBlackP1: './assets/tetrisboxer_black_p1.png',
@@ -15,6 +17,28 @@ const images = {
   boxerBlackP2: './assets/tetrisboxer_black_p2.png',
   boxerRedP2: './assets/tetrisboxer_red_p2.png',
   boxerBlueP2: './assets/tetrisboxer_blue_p2.png',
+  hpBar100p1: './assets/hpBar100p1.png',
+  hpBar90p1: './assets/hpBar90p1.png',
+  hpBar80p1: './assets/hpBar80p1.png',
+  hpBar70p1: './assets/hpBar70p1.png',
+  hpBar60p1: './assets/hpBar60p1.png',
+  hpBar50p1: './assets/hpBar50p1.png',
+  hpBar40p1: './assets/hpBar40p1.png',
+  hpBar30p1: './assets/hpBar30p1.png',
+  hpBar20p1: './assets/hpBar20p1.png',
+  hpBar10p1: './assets/hpBar10p1.png',
+  hpBar00p1: './assets/hpBar00p1.png',
+  hpBar100p2: './assets/hpBar100p2.png',
+  hpBar90p2: './assets/hpBar90p2.png',
+  hpBar80p2: './assets/hpBar80p2.png',
+  hpBar70p2: './assets/hpBar70p2.png',
+  hpBar60p2: './assets/hpBar60p2.png',
+  hpBar50p2: './assets/hpBar50p2.png',
+  hpBar40p2: './assets/hpBar40p2.png',
+  hpBar30p2: './assets/hpBar30p2.png',
+  hpBar20p2: './assets/hpBar20p2.png',
+  hpBar10p2: './assets/hpBar10p2.png',
+  hpBar00p2: './assets/hpBar00p2.png',
 }
 
 let p1RedSelected = {
@@ -24,8 +48,8 @@ let p1RedSelected = {
   height: 80,
   attack: 5,
   scope: 150,
-  defese: 4,
-  healPoints: 60,
+  defense: 4,
+  healPoints: 600,
   speed: 30,
   imageSrc: images.boxerRedP1,
 }
@@ -36,8 +60,8 @@ let p2RedSelected = {
   height: 80,
   attack: 5,
   scope: 150,
-  defese: 4,
-  healPoints: 60,
+  defense: 4,
+  healPoints: 600,
   speed: 30,
   imageSrc: images.boxerRedP2,
 }
@@ -48,8 +72,8 @@ let p1BlueSelected = {
   height: 90,
   attack: 8,
   scope: 100,
-  defese: 7,
-  healPoints: 90,
+  defense: 7,
+  healPoints: 900,
   speed: 15,
   imageSrc: images.boxerBlueP1,
 }
@@ -60,8 +84,8 @@ let p2BlueSelected = {
   height: 90,
   attack: 8,
   scope: 100,
-  defese: 7,
-  healPoints: 90,
+  defense: 7,
+  healPoints: 900,
   speed: 20,
   imageSrc: images.boxerBlueP2,
 }
@@ -72,8 +96,8 @@ let p1BlackSelected = {
   height: 100,
   attack: 15,
   scope: 70,
-  defese: 10,
-  healPoints: 130,
+  defense: 10,
+  healPoints: 1300,
   speed: 10,
   imageSrc: images.boxerBlackP1,
 }
@@ -84,22 +108,23 @@ let p2BlackSelected = {
   height: 100,
   attack: 15,
   scope: 70,
-  defese: 10,
-  healPoints: 130,
+  defense: 10,
+  healPoints: 1300,
   speed: 10,
   imageSrc: images.boxerBlackP2,
 }
 
 class BoxerP1 {
-  constructor(x, y, width, height, attack, scope, defese, healPoints, speed, imageSrc) {
+  constructor(x, y, width, height, attack, scope, defense, healPoints, speed, imageSrc) {
     this.x = x
     this.y = y
     this.width = width
     this.height = height
     this.attack = attack
     this.scope = scope
-    this.defese = defese
+    this.defense = defense
     this.healPoints = healPoints
+    this.remainingHp = healPoints
     this.speed = speed
     this.boxerImage = new Image()
     this.boxerImage.src = imageSrc
@@ -109,15 +134,16 @@ class BoxerP1 {
 }
 
 class BoxerP2 {
-  constructor(x, y, width, height, attack, scope, defese, healPoints, speed, imageSrc) {
+  constructor(x, y, width, height, attack, scope, defense, healPoints, speed, imageSrc) {
     this.x = x
     this.y = y
     this.width = width
     this.height = height
     this.attack = attack
     this.scope = scope
-    this.defese = defese
+    this.defense = defense
     this.healPoints = healPoints
+    this.remainingHp = healPoints
     this.speed = speed
     this.boxerImage = new Image()
     this.boxerImage.src = imageSrc
@@ -136,8 +162,9 @@ if (p1Selected === 'red') {
   p1Boxer.height = p1RedSelected.height
   p1Boxer.attack = p1RedSelected.attack
   p1Boxer.scope = p1RedSelected.scope
-  p1Boxer.defense = p1RedSelected.defese
-  p1Boxer.healPoints = p1RedSelected.height
+  p1Boxer.defense = p1RedSelected.defense
+  p1Boxer.healPoints = p1RedSelected.healPoints
+  p1Boxer.remainingHp = p1RedSelected.healPoints
   p1Boxer.speed = p1RedSelected.speed
   p1Boxer.boxerImage.src = p1RedSelected.imageSrc
 } else if (p1Selected === 'blue') {
@@ -147,8 +174,8 @@ if (p1Selected === 'red') {
   p1Boxer.height = p1BlueSelected.height
   p1Boxer.attack = p1BlueSelected.attack
   p1Boxer.scope = p1BlueSelected.scope
-  p1Boxer.defense = p1BlueSelected.defese
-  p1Boxer.healPoints = p1BlueSelected.height
+  p1Boxer.defense = p1BlueSelected.defense
+  p1Boxer.healPoints = p1BlueSelected.healPoints
   p1Boxer.speed = p1BlueSelected.speed
   p1Boxer.boxerImage.src = p1BlueSelected.imageSrc
 } else if (p1Selected === 'black') {
@@ -158,8 +185,8 @@ if (p1Selected === 'red') {
   p1Boxer.height = p1BlackSelected.height
   p1Boxer.attack = p1BlackSelected.attack
   p1Boxer.scope = p1BlackSelected.scope
-  p1Boxer.defense = p1BlackSelected.defese
-  p1Boxer.healPoints = p1BlackSelected.height
+  p1Boxer.defense = p1BlackSelected.defense
+  p1Boxer.healPoints = p1BlackSelected.healPoints
   p1Boxer.speed = p1BlackSelected.speed
   p1Boxer.boxerImage.src = p1BlackSelected.imageSrc
 }
@@ -172,8 +199,9 @@ if (p2Selected === 'red') {
   p2Boxer.height = p2RedSelected.height
   p2Boxer.attack = p2RedSelected.attack
   p2Boxer.scope = p2RedSelected.scope
-  p2Boxer.defense = p2RedSelected.defese
-  p2Boxer.healPoints = p2RedSelected.height
+  p2Boxer.defense = p2RedSelected.defense
+  p2Boxer.healPoints = p2RedSelected.healPoints
+  p2Boxer.remainingHp = p2RedSelected.healPoints
   p2Boxer.speed = p2RedSelected.speed
   p2Boxer.boxerImage.src = p2RedSelected.imageSrc
 } else if (p2Selected === 'blue') {
@@ -183,8 +211,9 @@ if (p2Selected === 'red') {
   p2Boxer.height = p2BlueSelected.height
   p2Boxer.attack = p2BlueSelected.attack
   p2Boxer.scope = p2BlueSelected.scope
-  p2Boxer.defense = p2BlueSelected.defese
-  p2Boxer.healPoints = p2BlueSelected.height
+  p2Boxer.defense = p2BlueSelected.defense
+  p2Boxer.healPoints = p2BlueSelected.healPoints
+  p2Boxer.remainingHp = p2BlueSelected.healPoints
   p2Boxer.speed = p2BlueSelected.speed
   p2Boxer.boxerImage.src = p2BlueSelected.imageSrc
 } else if (p2Selected === 'black') {
@@ -194,8 +223,9 @@ if (p2Selected === 'red') {
   p2Boxer.height = p2BlackSelected.height
   p2Boxer.attack = p2BlackSelected.attack
   p2Boxer.scope = p2BlackSelected.scope
-  p2Boxer.defense = p2BlackSelected.defese
-  p2Boxer.healPoints = p2BlackSelected.height
+  p2Boxer.defense = p2BlackSelected.defense
+  p2Boxer.healPoints = p2BlackSelected.healPoints
+  p2Boxer.remainingHp = p2BlackSelected.healPoints
   p2Boxer.speed = p2BlackSelected.speed
   p2Boxer.boxerImage.src = p2BlackSelected.imageSrc
 }
@@ -209,10 +239,141 @@ function update() {
   moveBoxer2()
   attackBoxer1()
   attackBoxer2()
-  //isPunchingP1Up()
+  drawP1HpBar(percentageHpP2)
+  drawP2HpBar(percentageHpP1)
 }
 
 setInterval(update, 1000 / 60)
+
+function drawP1HpBar(remainingHp) {
+  if (remainingHp <= 100 && remainingHp >= 90) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar100p1 = new Image()
+    hpBar100p1.src = images.hpBar100p1
+    context.drawImage(hpBar100p1, 80, 50, 250, 43)
+  } else if (remainingHp < 90 && remainingHp >= 80) {
+    //     context.clearRect(0, 0, 720, 480)
+    const hpBar90p1 = new Image()
+    hpBar90p1.src = images.hpBar90p1
+    context.drawImage(hpBar90p1, 80, 50, 250, 43)
+  } else if (remainingHp < 80 && remainingHp >= 70) {
+    //     context.clearRect(0, 0, 720, 480)
+    const hpBar80p1 = new Image()
+    hpBar80p1.src = images.hpBar80p1
+    context.drawImage(hpBar80p1, 80, 50, 250, 43)
+  } else if (remainingHp < 70 && remainingHp >= 60) {
+    //     context.clearRect(0, 0, 720, 480)
+    const hpBar70p1 = new Image()
+    hpBar70p1.src = images.hpBar70p1
+    context.drawImage(hpBar70p1, 80, 50, 250, 43)
+  } else if (remainingHp < 60 && remainingHp >= 50) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar60p1 = new Image()
+    hpBar60p1.src = images.hpBar60p1
+    context.drawImage(hpBar60p1, 80, 50, 250, 43)
+  } else if (remainingHp < 50 && remainingHp >= 40) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar50p1 = new Image()
+    hpBar50p1.src = images.hpBar50p1
+    context.drawImage(hpBar50p1, 80, 50, 250, 43)
+  } else if (remainingHp < 40 && remainingHp >= 30) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar40p1 = new Image()
+    hpBar40p1.src = images.hpBar40p1
+    context.drawImage(hpBar40p1, 80, 50, 250, 43)
+  } else if (remainingHp < 30 && remainingHp >= 20) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar30p1 = new Image()
+    hpBar30p1.src = images.hpBar30p1
+    context.drawImage(hpBar30p1, 80, 50, 250, 43)
+  } else if (remainingHp < 20 && remainingHp >= 10) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar20p1 = new Image()
+    hpBar20p1.src = images.hpBar20p1
+    context.drawImage(hpBar20p1, 80, 50, 250, 43)
+  } else if (remainingHp < 10 && remainingHp > 0) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar10p1 = new Image()
+    hpBar10p1.src = images.hpBar10p1
+    context.drawImage(hpBar10p1, 80, 50, 250, 43)
+  } else if (remainingHp <= 0) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar00p1 = new Image()
+    hpBar00p1.src = images.hpBar00p1
+    context.drawImage(hpBar00p1, 80, 50, 250, 43)
+  }
+}
+
+function drawP2HpBar(remainingHp) {
+  if (remainingHp <= 100 && remainingHp >= 90) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar100p2 = new Image()
+    hpBar100p2.src = images.hpBar100p2
+    context.drawImage(hpBar100p2, 390, 50, 250, 43)
+  } else if (remainingHp < 90 && remainingHp >= 80) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar90p2 = new Image()
+    hpBar90p2.src = images.hpBar90p2
+    context.drawImage(hpBar90p2, 390, 50, 250, 43)
+  } else if (remainingHp < 80 && remainingHp >= 70) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar80p2 = new Image()
+    hpBar80p2.src = images.hpBar80p2
+    context.drawImage(hpBar80p2, 390, 50, 250, 43)
+  } else if (remainingHp < 70 && remainingHp >= 60) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar70p2 = new Image()
+    hpBar70p2.src = images.hpBar70p2
+    context.drawImage(hpBar70p2, 390, 50, 250, 43)
+  } else if (remainingHp < 60 && remainingHp >= 50) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar60p2 = new Image()
+    hpBar60p2.src = images.hpBar60p2
+    context.drawImage(hpBar60p2, 390, 50, 250, 43)
+  } else if (remainingHp < 50 && remainingHp >= 40) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar50p2 = new Image()
+    hpBar50p2.src = images.hpBar50p2
+    context.drawImage(hpBar50p2, 390, 50, 250, 43)
+  } else if (remainingHp < 40 && remainingHp >= 30) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar40p2 = new Image()
+    hpBar40p2.src = images.hpBar40p2
+    context.drawImage(hpBar40p2, 390, 50, 250, 43)
+  } else if (remainingHp < 30 && remainingHp >= 20) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar30p2 = new Image()
+    hpBar30p2.src = images.hpBar30p2
+    context.drawImage(hpBar30p2, 390, 50, 250, 43)
+  } else if (remainingHp < 20 && remainingHp >= 10) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar20p2 = new Image()
+    hpBar20p2.src = images.hpBar20p2
+    context.drawImage(hpBar20p2, 390, 50, 250, 43)
+  } else if (remainingHp < 10 && remainingHp > 0) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar10p2 = new Image()
+    hpBar10p2.src = images.hpBar10p2
+    context.drawImage(hpBar10p2, 390, 50, 250, 43)
+  } else if (remainingHp <= 0) {
+    //context.clearRect(0, 0, 720, 480)
+    const hpBar00p2 = new Image()
+    hpBar00p2.src = images.hpBar00p2
+    context.drawImage(hpBar00p2, 390, 50, 250, 43)
+  }
+}
+
+function calculateDamageP1() {
+  p2Boxer.remainingHp = p2Boxer.remainingHp - p1Boxer.attack * (p2Boxer.defense / 10)
+  percentageHpP1 = (p2Boxer.remainingHp * 100) / p2Boxer.healPoints
+  return percentageHpP1
+}
+
+function calculateDamageP2() {
+  p1Boxer.remainingHp = p1Boxer.remainingHp - p2Boxer.attack * (p1Boxer.defense / 10)
+  percentageHpP2 = (p1Boxer.remainingHp * 100) / p1Boxer.healPoints
+  return percentageHpP2
+}
 
 function isRedP1PunchingArmUp() {
   if (
@@ -222,6 +383,7 @@ function isRedP1PunchingArmUp() {
     p1Boxer.y + 10 > p2Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP1()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -235,6 +397,7 @@ function isBlueP1PunchingArmUp() {
     p1Boxer.y + 10 > p2Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP1()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -248,6 +411,7 @@ function isBlackP1PunchingArmUp() {
     p1Boxer.y + 3 + 11 > p2Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP1()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -261,6 +425,7 @@ function isRedP1PunchingArmDown() {
     p1Boxer.y + p1Boxer.width + 10 + 10 > p2Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP1()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -274,6 +439,7 @@ function isBlueP1PunchingArmDown() {
     p1Boxer.y + p1Boxer.width + 10 + 10 > p2Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP1()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -287,6 +453,7 @@ function isBlackP1PunchingArmDown() {
     p1Boxer.y + p1Boxer.width + 6 + 11 > p2Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP1()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -359,6 +526,7 @@ function isRedP2PunchingArmUp() {
     p2Boxer.y + 10 > p1Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP2()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -372,6 +540,7 @@ function isBlueP2PunchingArmUp() {
     p2Boxer.y + 10 > p1Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP2()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -385,6 +554,7 @@ function isBlackP2PunchingArmUp() {
     p2Boxer.y + 3 + 11 > p1Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP2()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -398,6 +568,7 @@ function isRedP2PunchingArmDown() {
     p2Boxer.y + p2Boxer.height - 10 + 10 > p1Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP2()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -411,6 +582,7 @@ function isBlueP2PunchingArmDown() {
     p2Boxer.y + p2Boxer.height - 10 + 10 > p1Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP2()
   } else {
     console.log(`NO Esta Pegando`)
   }
@@ -424,6 +596,7 @@ function isBlackP2PunchingArmDown() {
     p2Boxer.y + p2Boxer.height - 14 + 11 > p1Boxer.y
   ) {
     console.log(`Esta Pegando`)
+    calculateDamageP2()
   } else {
     console.log(`NO Esta Pegando`)
   }

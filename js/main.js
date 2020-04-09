ea17d4b3c93fb0ad43032cddadfe42e2df78c7c9
Vidal Diaz <vgdiaz@hotmail.com>
2020-04-09 00:49:21 -0500

    Se crea funcionalidad para seleccionar el color del Boxer y que se ajusten los brazos a la figura de Boxer

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ const context = canvas.getContext('2d')
 const friction = 0.8
 const keys = []
 
+let p1Selected = 'black'
+let p2Selected = 'black'
+//let p2Selected
+
 const images = {
   boxerBlackP1: './assets/tetrisboxer_black_p1.png',
   boxerRedP1: './assets/tetrisboxer_red_p1.png',
@@ -11,6 +15,79 @@ const images = {
   boxerBlackP2: './assets/tetrisboxer_black_p2.png',
   boxerRedP2: './assets/tetrisboxer_red_p2.png',
   boxerBlueP2: './assets/tetrisboxer_blue_p2.png',
+}
+
+let p1RedSelected = {
+  x: 20,
+  y: 20,
+  width: 60,
+  height: 80,
+  attack: 8,
+  scope: 50,
+  defese: 4,
+  healPoints: 80,
+  speed: 3,
+  imageSrc: images.boxerRedP1,
+}
+let p2RedSelected = {
+  x: 620,
+  y: 360,
+  width: 60,
+  height: 80,
+  attack: 8,
+  scope: 50,
+  defese: 4,
+  healPoints: 80,
+  speed: 3,
+  imageSrc: images.boxerRedP2,
+}
+let p1BlueSelected = {
+  x: 20,
+  y: 20,
+  width: 70,
+  height: 90,
+  attack: 12,
+  scope: 20,
+  defese: 6,
+  healPoints: 90,
+  speed: 4,
+  imageSrc: images.boxerBlueP1,
+}
+let p2BlueSelected = {
+  x: 620,
+  y: 360,
+  width: 70,
+  height: 90,
+  attack: 12,
+  scope: 20,
+  defese: 6,
+  healPoints: 90,
+  speed: 4,
+  imageSrc: images.boxerBlueP2,
+}
+let p1BlackSelected = {
+  x: 20,
+  y: 20,
+  width: 80,
+  height: 100,
+  attack: 10,
+  scope: 35,
+  defese: 8,
+  healPoints: 100,
+  speed: 3,
+  imageSrc: images.boxerBlackP1,
+}
+let p2BlackSelected = {
+  x: 620,
+  y: 360,
+  width: 80,
+  height: 100,
+  attack: 10,
+  scope: 35,
+  defese: 8,
+  healPoints: 100,
+  speed: 3,
+  imageSrc: images.boxerBlackP2,
 }
 
 class BoxerP1 {
@@ -51,15 +128,83 @@ class BoxerP2 {
   }
 }
 
-let p1Boxer = new BoxerP1(20, 20, 60, 80, 8, 'large', 4, 80, 3, images.boxerRedP1)
-//let p2Boxer = new BoxerP2(620, 360, 80, 100, 10, 'medium', 8, 100, 3, images.boxerBlackP2)
-let p2Boxer = new BoxerP2(620, 360, 70, 90, 12, 'slow', 6, 90, 4, images.boxerBlueP2)
+let p1Boxer = new BoxerP1(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+if (p1Selected === 'red') {
+  p1Boxer.x = p1RedSelected.x
+  p1Boxer.y = p1RedSelected.y
+  p1Boxer.width = p1RedSelected.width
+  p1Boxer.height = p1RedSelected.height
+  p1Boxer.attack = p1RedSelected.attack
+  p1Boxer.scope = p1RedSelected.scope
+  p1Boxer.defense = p1RedSelected.defese
+  p1Boxer.healPoints = p1RedSelected.height
+  p1Boxer.speed = p1RedSelected.speed
+  p1Boxer.boxerImage.src = p1RedSelected.imageSrc
+} else if (p1Selected === 'blue') {
+  p1Boxer.x = p1BlueSelected.x
+  p1Boxer.y = p1BlueSelected.y
+  p1Boxer.width = p1BlueSelected.width
+  p1Boxer.height = p1BlueSelected.height
+  p1Boxer.attack = p1BlueSelected.attack
+  p1Boxer.scope = p1BlueSelected.scope
+  p1Boxer.defense = p1BlueSelected.defese
+  p1Boxer.healPoints = p1BlueSelected.height
+  p1Boxer.speed = p1BlueSelected.speed
+  p1Boxer.boxerImage.src = p1BlueSelected.imageSrc
+} else if (p1Selected === 'black') {
+  p1Boxer.x = p1BlackSelected.x
+  p1Boxer.y = p1BlackSelected.y
+  p1Boxer.width = p1BlackSelected.width
+  p1Boxer.height = p1BlackSelected.height
+  p1Boxer.attack = p1BlackSelected.attack
+  p1Boxer.scope = p1BlackSelected.scope
+  p1Boxer.defense = p1BlackSelected.defese
+  p1Boxer.healPoints = p1BlackSelected.height
+  p1Boxer.speed = p1BlackSelected.speed
+  p1Boxer.boxerImage.src = p1BlackSelected.imageSrc
+}
+
+let p2Boxer = new BoxerP2(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+if (p2Selected === 'red') {
+  p2Boxer.x = p2RedSelected.x
+  p2Boxer.y = p2RedSelected.y
+  p2Boxer.width = p2RedSelected.width
+  p2Boxer.height = p2RedSelected.height
+  p2Boxer.attack = p2RedSelected.attack
+  p2Boxer.scope = p2RedSelected.scope
+  p2Boxer.defense = p2RedSelected.defese
+  p2Boxer.healPoints = p2RedSelected.height
+  p2Boxer.speed = p2RedSelected.speed
+  p2Boxer.boxerImage.src = p2RedSelected.imageSrc
+} else if (p2Selected === 'blue') {
+  p2Boxer.x = p2BlueSelected.x
+  p2Boxer.y = p2BlueSelected.y
+  p2Boxer.width = p2BlueSelected.width
+  p2Boxer.height = p2BlueSelected.height
+  p2Boxer.attack = p2BlueSelected.attack
+  p2Boxer.scope = p2BlueSelected.scope
+  p2Boxer.defense = p2BlueSelected.defese
+  p2Boxer.healPoints = p2BlueSelected.height
+  p2Boxer.speed = p2BlueSelected.speed
+  p2Boxer.boxerImage.src = p2BlueSelected.imageSrc
+} else if (p2Selected === 'black') {
+  p2Boxer.x = p2BlackSelected.x
+  p2Boxer.y = p2BlackSelected.y
+  p2Boxer.width = p2BlackSelected.width
+  p2Boxer.height = p2BlackSelected.height
+  p2Boxer.attack = p2BlackSelected.attack
+  p2Boxer.scope = p2BlackSelected.scope
+  p2Boxer.defense = p2BlackSelected.defese
+  p2Boxer.healPoints = p2BlackSelected.height
+  p2Boxer.speed = p2BlackSelected.speed
+  p2Boxer.boxerImage.src = p2BlackSelected.imageSrc
+}
 
 //Funciones Principales
 function update() {
   context.clearRect(0, 0, canvas.width, canvas.height)
-  drawP1Boxer(p1Boxer)
-  drawP2Boxer(p2Boxer)
+  drawP1Boxer()
+  drawP2Boxer()
   moveBoxer1()
   moveBoxer2()
   attackBoxer1()
@@ -70,55 +215,92 @@ setInterval(update, 1000 / 60)
 
 function attackBoxer1() {
   if (keys[84]) {
+    //Brazo Arriba P1
     p1Boxer.scope = 0
-    p1Boxer.scope += 100
-    context.fillStyle = '#EA3C63'
-    context.fillRect(p1Boxer.x + p1Boxer.width - 10, p1Boxer.y, 10 + 100, 10)
+    if (p1Selected === 'red') {
+      p1Boxer.scope += p1RedSelected.scope
+      context.fillStyle = '#EA3C63' //Color Rojo
+      context.fillRect(p1Boxer.x + p1Boxer.width - 10, p1Boxer.y, p1Boxer.scope, 10)
+    } else if (p1Selected === 'blue') {
+      p1Boxer.scope += p1BlueSelected.scope
+      context.fillStyle = '#3EAAF4' //Color Azul
+      context.fillRect(p1Boxer.x + p1Boxer.width - 3, p1Boxer.y, p1Boxer.scope, 10)
+    } else if (p1Selected === 'black') {
+      p1Boxer.scope += p1BlackSelected.scope
+      context.fillStyle = 'black'
+      context.fillRect(p1Boxer.x + p1Boxer.width - 9, p1Boxer.y + 3, p1Boxer.scope, 11)
+    }
   }
 
   if (keys[71]) {
+    //Brazo Abajo P1
     p1Boxer.scope = 0
-    p1Boxer.scope += 100
-    context.fillStyle = '#EA3C63'
-    context.fillRect(
-      p1Boxer.x + p1Boxer.width - 10,
-      p1Boxer.y + p1Boxer.height - 10,
-      10 + p1Boxer.scope,
-      10
-    )
+    if (p1Selected === 'red') {
+      p1Boxer.scope += p1RedSelected.scope
+      context.fillStyle = '#EA3C63' //Color Rojo
+      context.fillRect(
+        p1Boxer.x + p1Boxer.width - 10,
+        p1Boxer.y + p1Boxer.width + 10,
+        p1Boxer.scope,
+        10
+      )
+    } else if (p1Selected === 'blue') {
+      p1Boxer.scope += p1BlueSelected.scope
+      context.fillStyle = '#3EAAF4' //Color Azul
+      context.fillRect(
+        p1Boxer.x + p1Boxer.width - 3,
+        p1Boxer.y + p1Boxer.width + 10,
+        p1Boxer.scope,
+        10
+      )
+    } else if (p1Selected === 'black') {
+      p1Boxer.scope += p1BlackSelected.scope
+      context.fillStyle = 'black'
+      context.fillRect(
+        p1Boxer.x + p1Boxer.width - 11,
+        p1Boxer.y + p1Boxer.width + 6,
+        p1Boxer.scope,
+        11
+      )
+    }
   }
 }
 
-// function attackBoxer2() { //Negro
-//   if (keys[79]) {
-//     p2Boxer.scope = 0
-//     p2Boxer.scope += 200
-//     context.fillStyle = 'black'
-//     context.fillRect(p2Boxer.x + 10, p2Boxer.y + 3, 10 - p2Boxer.scope, 10)
-//   }
-
-//   if (keys[76]) {
-//     p2Boxer.scope = 0
-//     p2Boxer.scope += 200
-//     context.fillStyle = 'black'
-//     context.fillRect(p2Boxer.x + 10, p2Boxer.y + p2Boxer.width + 7, 10 - p2Boxer.scope, 10)
-//   }
-// }
-
 function attackBoxer2() {
-  //Azul
   if (keys[79]) {
+    //Brazo Arriba P2
     p2Boxer.scope = 0
-    p2Boxer.scope += 100
-    context.fillStyle = '#3EAAF4'
-    context.fillRect(p2Boxer.x + 3, p2Boxer.y, 10 - p2Boxer.scope, 10)
+    if (p2Selected === 'red') {
+      p2Boxer.scope += p2RedSelected.scope
+      context.fillStyle = '#EA3C63'
+      context.fillRect(p2Boxer.x + 4, p2Boxer.y, -p2Boxer.scope, 10)
+    } else if (p2Selected === 'blue') {
+      p2Boxer.scope += p2BlueSelected.scope
+      context.fillStyle = '#3EAAF4'
+      context.fillRect(p2Boxer.x + 3, p2Boxer.y, -p2Boxer.scope, 10)
+    } else if (p2Selected === 'black') {
+      p2Boxer.scope += p2BlackSelected.scope
+      context.fillStyle = 'black'
+      context.fillRect(p2Boxer.x + 10, p2Boxer.y + 3, -p2Boxer.scope, 11)
+    }
   }
 
   if (keys[76]) {
+    //Brazo Abajo P2
     p2Boxer.scope = 0
-    p2Boxer.scope += 100
-    context.fillStyle = '#3EAAF4'
-    context.fillRect(p2Boxer.x + 3, p2Boxer.y + p2Boxer.width + 10, 10 - p2Boxer.scope, 10)
+    if (p2Selected === 'red') {
+      p2Boxer.scope += p2RedSelected.scope
+      context.fillStyle = '#EA3C63'
+      context.fillRect(p2Boxer.x + 5, p2Boxer.y + p2Boxer.height - 10, -p2Boxer.scope, 10)
+    } else if (p2Selected === 'blue') {
+      p2Boxer.scope += p2BlueSelected.scope
+      context.fillStyle = '#3EAAF4'
+      context.fillRect(p2Boxer.x + 3, p2Boxer.y + p2Boxer.height - 10, -p2Boxer.scope, 10)
+    } else if (p2Selected === 'black') {
+      p2Boxer.scope += p2BlackSelected.scope
+      context.fillStyle = 'black'
+      context.fillRect(p2Boxer.x + 10, p2Boxer.y + p2Boxer.height - 14, -p2Boxer.scope, 11)
+    }
   }
 }
 
@@ -187,6 +369,7 @@ function moveBoxer2() {
 }
 
 function drawP1Boxer() {
+  //context.drawImage(p1Boxer.boxerImage, p1Boxer.x, p1Boxer.y, p1Boxer.width, p1Boxer.height)
   context.drawImage(p1Boxer.boxerImage, p1Boxer.x, p1Boxer.y, p1Boxer.width, p1Boxer.height)
 }
 
@@ -194,7 +377,12 @@ function drawP2Boxer() {
   context.drawImage(p2Boxer.boxerImage, p2Boxer.x, p2Boxer.y, p2Boxer.width, p2Boxer.height)
 }
 
-// isTouchingP1Down(x, y, w, h) {
+// function isTouchingP1Up(p1x, p1y, p1width, p1height, p2x, p2y, p2width, p2height) {
+//   return p1x < p2x + p2width && p1x + p1width > p2x && p1y < p2y + p2height && p1y + p1height > p2y
+//   console.log(`Hay una colision entre Boxeadores`)
+// }
+
+// isTouching(pipe) {
 //   return (
 //     this.x < pipe.x + pipe.width &&
 //     this.x + this.width > pipe.x &&
